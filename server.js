4ab6ec4dfaa5fd61539690f4235a2a5096526a8e
Duncan Jauncey <duncan.jauncey@gmail.com>
2012-12-17 12:28:06 +0000

    Update server.js

--- server.js
+++ server.js
@@ -2,5 +2,5 @@ var http = require('http');
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain'});
   res.end('Hello World, this is the server\n');
-}).listen(8150);
-console.log('Server running on port 8150');
+}).listen(80);
+console.log('Server running on port 80');
