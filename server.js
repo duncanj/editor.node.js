d2f2dd4266260011ff840a62e6e21323dc9044ac
Duncan Jauncey <duncan.jauncey@gmail.com>
2012-12-17 13:11:01 +0000

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,25 @@
-var http = require('http');
+var http = require('http'), 
+ 	fs = require("fs");
 http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello World, this is the server\n');
+  if( req.url == '@process.log' ) {
+    var logfile = '/var/tmp/editor.node.js-master-server.log';
+ 	  fs.readFile(filename, "binary", function(err, file) {
+	    if(err) {
+        response.writeHead(500, {"Content-Type": "text/plain"});
+        response.write(err + "\n");
+        response.end();
+        return;
+      }
+
+      res.writeHead(200, {'Content-Type': 'text/plain'});
+      response.write(file, "binary");
+      response.end();
+
+    });    
+    
+  } else {
+  	res.writeHead(200, {'Content-Type': 'text/plain'});
+  	res.end('Hello World, this is the server\n');
+  }
 }).listen(80);
 console.log('Server running on port 80, hopefully');
